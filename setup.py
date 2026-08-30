fc2d8d1378865feedcc8456d1d7c4395a4d5e586
naseraljed-star <naser.aljed@gmail.com>
2026-08-30 21:17:11 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-"""
-Setup instructions:
-Run this script to configure your local environment.
-You will be asked to enter a password during setup.
-"""
-def greet(name):
-    return "Hello, " + name
